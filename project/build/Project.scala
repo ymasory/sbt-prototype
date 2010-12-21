cfaab50d38572fc6654de3df79d94e5f1c3cb3cd
Yuvi Masory <ymasory@gmail.com>
2010-12-20 21:22:30 -0500

    add basic configs

---
+++ project/build/Project.scala
@@ -0,0 +1,32 @@
+import sbt._
+
+class Project(info: ProjectInfo) extends DefaultProject(info) with ProguardProject with Exec {
+  
+  //project name
+  override val artifactID = "myproj"
+
+  //managed dependencies from built-in repositories
+  // val scalatest = "org.scalatest" % "scalatest" % "1.2"
+  // val jsap = "com.martiansoftware" % "jsap" % "2.1"
+  
+  //files to go in packaged jars
+  val extraResources = "README.md" +++ "LICENSE"
+  override val mainResources = super.mainResources +++ extraResources
+
+  //program entry point
+  override def mainClass: Option[String] = Some("com.yuvimasory.myproj.Main")
+
+  //proguard
+  override def proguardOptions = List(
+    "-keepclasseswithmembers public class * { public static void main(java.lang.String[]); }",
+    "-dontoptimize",
+    "-dontobfuscate",
+    proguardKeepLimitedSerializability,
+    proguardKeepAllScala,
+    "-keep interface scala.ScalaObject"
+  )
+  override def proguardInJars = Path.fromFile(scalaLibraryJar) +++ super.proguardInJars
+
+  override def compileOptions = super.compileOptions ++ Seq(Deprecation, Unchecked) //ExplainTypes
+  override def documentOptions = super.documentOptions ++ Seq(LinkSource)
+}
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+package com.yuvimasory.myproj
+
+object Main {
+
+  def main(args: Array[String]) {
+    println("hello myproj")
+  }
+}
