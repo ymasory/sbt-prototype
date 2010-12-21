4e91b138390b76cb0706502ebfdff9e4119552cc
Yuvi Masory <ymasory@gmail.com>
2010-12-20 21:32:48 -0500

    simplify proguard opts

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -16,17 +16,13 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with ProguardProje
   //program entry point
   override def mainClass: Option[String] = Some("com.yuvimasory.myproj.Main")
 
+  override def compileOptions = super.compileOptions ++ Seq(Deprecation, Unchecked) //ExplainTypes
+  override def documentOptions = super.documentOptions ++ Seq(LinkSource)
+
   //proguard
   override def proguardOptions = List(
     "-keepclasseswithmembers public class * { public static void main(java.lang.String[]); }",
-    "-dontoptimize",
-    "-dontobfuscate",
-    proguardKeepLimitedSerializability,
-    proguardKeepAllScala,
-    "-keep interface scala.ScalaObject"
+    proguardKeepAllScala
   )
   override def proguardInJars = Path.fromFile(scalaLibraryJar) +++ super.proguardInJars
-
-  override def compileOptions = super.compileOptions ++ Seq(Deprecation, Unchecked) //ExplainTypes
-  override def documentOptions = super.documentOptions ++ Seq(LinkSource)
 }
