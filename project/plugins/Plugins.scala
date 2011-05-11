68fab27b5a1b7d59ebb3d16a684bee01fb5d5263
Yuvi Masory <ymasory@gmail.com>
2011-05-10 20:16:42 -0400

    use proguard 0.0.5

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -2,5 +2,5 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
 
-  val proguard = "org.scala-tools.sbt" % "sbt-proguard-plugin" % "0.0.4"
+  val proguard = "org.scala-tools.sbt" % "sbt-proguard-plugin" % "0.0.5"
 }
